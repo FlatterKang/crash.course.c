ab8f60e24f9b41fc61956fce72b00ba0e6c87a4c
zxj <zxjnt@126.com>
2019-07-16 17:45:05 +0800

    function

--- chapter05/example/list0508.c
+++ chapter05/example/list0508.c
@@ -2,25 +2,26 @@
     对数组的全部元素进行倒序排列
 */
 #include <stdio.h>
+#define NUMBER 6
 
 int main(void) {
     int i;
-    int x[7];
+    int x[NUMBER];
 
-    for (i = 0; i < 7; i++) {
+    for (i = 0; i < NUMBER; i++) {
         printf("x[%d]:", i);
         scanf("%d", &x[i]);
     }
 
-    for (i = 0; i < 3; i++) {
+    for (i = 0; i < NUMBER / 2; i++) {
         int temp = x[i];
-        x[i] = x[6 - i];
-        x[6 - i] = temp;
+        x[i] = x[NUMBER - 1 - i];
+        x[NUMBER - 1 - i] = temp;
     }
 
     puts("倒序排列了。");
 
-    for (i = 0; i < 7; i++) {
+    for (i = 0; i < NUMBER; i++) {
         printf("x[%d] = %d\n", i, x[i]);
     }
 
