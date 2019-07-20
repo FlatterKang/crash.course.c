632da758e1992e6eced167409819f909679e4eb5
Allen Guo <elon.guo@gmail.com>
2019-07-21 02:24:26 +0800

    update doxygen

--- sample-doxygen/sample.h
+++ sample-doxygen/sample.h
@@ -1,21 +1,17 @@
 /**
-* @file  CommonType.h  
-* @brief 常见类型定义
-* @author       Vincent
-* @date     2015-5-24 
-* @version  A001 
-* @copyright Vincent                                                              
-*/
-void print_one_line_csv(char *line);
+ * @file  sample.h  
+ * @brief 一个例子定义
+ * @author Allen Guo
+ * @date 2019-07-21
+ * @version  1.0
+ * @copyright Banyuan.club
+ */
 
 /** 
- * 读取文件
- * @param[in]    fileNameLen    文件名长度
- * @param[in]   fileName    文件名
- * @param[in]    dataLen        数据长度
- * @param[out]  fileData    输出数据
- * @return        0，执行成功，非0，失败，详见
- * @ref            RetCode.h
+ * 获取条目入口
+ * @param argc 文件名长度
+ * @param  argv 文件名
+ * @return 0，执行成功，非0，失败
  * @see
  * @note
  */ 
